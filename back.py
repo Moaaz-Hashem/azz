b3fef1706d4c551f8d43b75bbc93c4ce24c734b4
Moaaz-Hashem <113250374+Moaaz-Hashem@users.noreply.github.com>
2023-06-13 15:20:53 +0300

    back.py

--- back.py
+++ back.py
@@ -1,10 +1,9 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-	return "hello"
-
+   return render_template('index.html', message='Hello BEC employees')
 if __name__ == '__main__':
     app.run()
